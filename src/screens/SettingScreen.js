3d0afef4f69a9985e4bee16d5e60b8ce473c3fd9
Shelja-Sharma <shelja.sharma@chicmic.co.in>
2021-05-13 16:25:58 +0530

    only setting screen commit

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -6,7 +6,9 @@ const SettingScreen = ()=>{
     return(
         <View style ={{alignItems:"center",height:responsiveHeight(100)}}>
             <Image source = {require("../assets/images/image4.png")} style = {{width:responsiveWidth(30),height:responsiveHeight(30)}} resizeMode ='contain' />
+            <Text>Setting SettingScreen</Text>
         </View>
+
     )
 }
 
