513395a89a9e2b2158f0bedf84d063d06989f5dc
Shelja-Sharma <shelja.sharma@chicmic.co.in>
2021-05-13 17:01:03 +0530

    changes in checkscreen for testing purpose

--- src/screens/CheckScree.js
+++ src/screens/CheckScree.js
@@ -6,6 +6,7 @@ const CheckScreen = ()=>{
     return(
         <View style ={{alignItems:"center",height:responsiveHeight(100)}}>
         <Image source = {require("../assets/images/image2.png")} style = {{width:responsiveWidth(30),height:responsiveHeight(30)}} resizeMode ='contain' />
+        <Text>This is Image 2 in the source folder of the project. this line is only for testing purpose.</Text>
     </View>
     )
 }
